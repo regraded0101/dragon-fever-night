e85b44142226b5a5e55e3bbe31b1066918c54810
Jon Fuller <jonfuller0207@gmail.com>
2023-04-01 18:42:06 +0100

    revert back to local data

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import subprocess
 import streamlit as st
 import pandas as pd
 
-#data_month_year = pd.read_csv('data/france_regions_weather_data.csv')
+data_month_year = pd.read_csv('data/france_regions_weather_data.csv')
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+# @st.cache_data(ttl=600)
+# def load_data(sheets_url):
+#     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+#     return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+# df = load_data(st.secrets["public_gsheets_url"])
 
 
 convert_months = {
