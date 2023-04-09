38ffc01da09683d54aaeb28bc45a82522eacc123
Jon Fuller <jonfuller0207@gmail.com>
2023-04-09 12:22:19 +0100

    iterate over wine regions data there is deffo a better way to write the defensive programming than to assign None to failed cases

--- src/get_data.py
+++ src/get_data.py
@@ -1,4 +1,6 @@
 import csv
+import glob
+
 import pandas as pd
 from geopy.geocoders import Nominatim
 from meteostat import Stations, Daily
@@ -25,105 +27,113 @@ class FetchData:
         try:
             if len(station) == 0:
                 raise ValueError(
-                    f"`station` is empty, check the `search_distance` value is not too large`"
+                    f"No weather stations near {self.place} could be found.\nIgnoring, increase the `search_distance` argument to find weather stations.\nCurrent set at {search_distance}"
                 )
             return station
         except ValueError as error:
-            print(str(error))
+            print("Warning: ", str(error))
             pass
 
     def get_all_weather_data(self):
         # Get Daily data
         loc = self.get_lat_long()
         station_id = self.get_close_station_ids(loc, search_distance=75000)
+        if station_id is not None:
+            data_list = []
+            for j in range(len(station_id.index)):
+                point_data = Daily(
+                    station_id.index.values[j], self.start_date, self.end_date
+                )
+                point_data = point_data.fetch()
 
-        data_list = []
-        for j in range(len(station_id.index)):
-            point_data = Daily(
-                station_id.index.values[j], self.start_date, self.end_date
-            )
-            point_data = point_data.fetch()
+                point_data = point_data.reset_index()
+                point_data = point_data[["time", "tavg", "tmin", "tmax"]]
+                point_data["location"] = self.place
 
-            point_data = point_data.reset_index()
-            point_data = point_data[["time", "tavg", "tmin", "tmax"]]
-            point_data["location"] = self.place
+                # drop any rows where time, tav, tmin, tmax are NA
+                point_data = point_data[
+                    (~point_data["tavg"].isna())
+                    | (~point_data["tmin"].isna())
+                    | (~point_data["tmax"].isna())
+                    | (~point_data["time"].isna())
+                ]
 
-            # drop any rows where time, tav, tmin, tmax are NA
-            point_data = point_data[
-                (~point_data["tavg"].isna())
-                | (~point_data["tmin"].isna())
-                | (~point_data["tmax"].isna())
-                | (~point_data["time"].isna())
-            ]
+                data_list.append(point_data)
 
-            data_list.append(point_data)
-
-        return data_list
+            return data_list
+        else:
+            return None
 
     def combine_weather_data(self):
+
         weather_data_list = self.get_all_weather_data()
+        if weather_data_list is not None:
+            output_data = pd.DataFrame(columns=weather_data_list[0].columns)
 
-        output_data = pd.DataFrame(columns=weather_data_list[0].columns)
+            # create a list of all dates that can be removed when date has already been found
+            date_list = pd.date_range(start=self.start_date, end=self.end_date)
 
-        # create a list of all dates that can be removed when date has already been found
-        date_list = pd.date_range(start=self.start_date, end=self.end_date)
+            for k in range(len(weather_data_list)):
+                # only select values that are in the date_list so haven't yet been populated
+                weather_data_list[k] = weather_data_list[k][
+                    weather_data_list[k]["time"].isin(date_list)
+                ]
 
-        for k in range(len(weather_data_list)):
-            # only select values that are in the date_list so haven't yet been populated
-            weather_data_list[k] = weather_data_list[k][
-                weather_data_list[k]["time"].isin(date_list)
-            ]
+                output_data = pd.concat([output_data, weather_data_list[k]])
 
-            output_data = pd.concat([output_data, weather_data_list[k]])
+                # update date list to drop dates already included
+                date_list = date_list[~date_list.isin(weather_data_list[k]["time"])]
 
-            # update date list to drop dates already included
-            date_list = date_list[~date_list.isin(weather_data_list[k]["time"])]
-
-        return output_data
+            return output_data
+        else:
+            return None
 
 
 def get_summary_stats(data):
     """
     Function to create the average temperatures by month-year and just by month
     """
+    if data is not None:
+        data["year"] = data["time"].dt.year
+        data["month"] = data["time"].dt.month
+        data_month_year = (
+            data.groupby(["year", "month", "location"])[["tavg", "tmin", "tmax"]]
+            .mean()
+            .reset_index()
+        )
 
-    data["year"] = data["time"].dt.year
-    data["month"] = data["time"].dt.month
-    data_month_year = (
-        data.groupby(["year", "month", "location"])[["tavg", "tmin", "tmax"]]
-        .mean()
-        .reset_index()
-    )
+        data_month = (
+            data_month_year.groupby(["month", "location"])[["tavg", "tmin", "tmax"]]
+            .mean()
+            .reset_index()
+        )
+        data_month.columns = ["month", "location", "tavg_month", "tmin_month", "tmax_month"]
+        data_month_year = data_month_year.merge(
+            data_month, how="left", on=["month", "location"]
+        )
 
-    data_month = (
-        data_month_year.groupby(["month", "location"])[["tavg", "tmin", "tmax"]]
-        .mean()
-        .reset_index()
-    )
-    data_month.columns = ["month", "location", "tavg_month", "tmin_month", "tmax_month"]
-    data_month_year = data_month_year.merge(
-        data_month, how="left", on=["month", "location"]
-    )
-
-    return data_month_year
+        return data_month_year
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    with open("data/france-wine-regions.csv", newline="") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        places = [row[0] for row in reader]
-        places = places[1:]  # drop the header
+    # read in all -wine-regions.csv files in data/
+    pattern = "data/*-wine-regions.csv"
+    matches = glob.glob(pattern)
 
-    with open("data/italy-wine-regions.csv", newline="") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        places_italy = [row[0] for row in reader]
-        places_italy = places_italy[1:]  # drop the header
+    places = []
+    for match in matches:
+        with open(match, newline="") as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            match_places = [row[0] for row in reader]
+            match_places = match_places[1:]  # drop the header
+            places.extend(match_places)
 
     start = "2000-01-01"
     end = "2022-12-31"
     weather_data = pd.DataFrame()
 
-    places.extend(places_italy)
     for place in places:
         print(f"Attempting: {place}")
 
